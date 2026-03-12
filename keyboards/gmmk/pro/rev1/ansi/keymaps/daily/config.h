97378cc220b1efcaea282364a2c3d38009bf3d62
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2026-03-12 01:44:56 -0400

    add debounce time, rgb settings, additional encoder/knob settings based on https://www.reddit.com/r/glorious/comments/rxj1h8

--- keyboards/gmmk/pro/rev1/ansi/keymaps/daily/config.h
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/daily/config.h
@@ -17,16 +17,28 @@
 
 #pragma once
 
-#define DRIVER_1_LED_TOTAL 66
-#define DRIVER_2_LED_TOTAL 32
+#define DRIVER_1_LED_TOTAL 66 // GMMK Pro-specific LED mapping
+#define DRIVER_2_LED_TOTAL 32 // GMMK Pro-specific LED mapping
+#define DRIVER_LED_TOTAL (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
 
 //enable RGB Matrix Effects for Vial
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #define RGB_MATRIX_KEYPRESSES
+
+// Additional RGB settings
+// #define RGBLIGHT_HUE_STEP 5   // Smaller increments for finer color control, default 10
+// #define RGBLIGHT_SAT_STEP 10  // Lower number = smaller change per press, default 17
+// #define RGBLIGHT_VAL_STEP 10  // Lower number = smaller change per press, default 17
+#define RGB_DISABLE_WHEN_USB_SUSPENDED // turns off RGB when computer is off
 
 //Vial Keyboard UID
 #define VIAL_KEYBOARD_UID {0x03, 0x75, 0x3D, 0xEC, 0x97, 0xC2, 0xE9, 0x9A}
 #define VIAL_UNLOCK_COMBO_ROWS { 1, 10 }
 #define VIAL_UNLOCK_COMBO_COLS { 3, 4 }
 
+// Additional custom configs
+#define VIAL_ENCODER_DEFAULT { KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
+// - hardcoded fallback encoder/knob map
 #define FORCE_NKRO // keeb will not fallback to 6KRO, starts in NKRO
+#define DEBOUNCE 8 // debounce time in ms for asymmetrical eager/deferred debounce (see rules.mk DEBOUNCE_TYPE)
+// if inputs skipped, decrease debounce; if one press doubleinputs, increase
